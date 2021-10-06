22177d112ae56d06d74eade5457dac7be73fde3c
“ELOBO5” <elicialobo@hotmail.com>
2021-10-06 12:56:37 +0100

    test practice

---
+++ urlshorterner/shortener/conftest.py
@@ -0,0 +1,7 @@
+# import pytest
+# from django.test import Client, TestCase
+
+# @pytest.fixture
+# def api():
+#     api = Client()
+#     return api
---
+++ urlshorterner/shortener/test_.py
@@ -0,0 +1,10 @@
+# import json
+# from django.urls import reverse
+
+# # Create your tests here.
+# class TestAPICase():
+
+#     def test_home(self):
+#         res = self.api.get(reverse('home'))
+#         assert res.status == '200 OK'
+#         assert b'URL Shortener' in res.data
