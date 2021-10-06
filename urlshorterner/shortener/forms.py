f4863e1d296f0407d31abe8df16295ab719ea32c
“ELOBO5” <elicialobo@hotmail.com>
2021-10-06 11:51:43 +0100

    clean files, add readme.md

--- urlshorterner/shortener/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Shortener
-
-class ShortenerForm(forms.ModelForm):
-    long_url = forms.URLField()
-
-    class Meta:
-        model = Shortener
-        fields = ['long_url']
--- urlshorterner/shortener/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('<str:short_url>/', views.url_redirect, name='redirect')
-]
